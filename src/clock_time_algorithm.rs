3bcdf4962b422b0d5c98287edcbfbdf963c60e0c
JonasJore <jonas.jore@gmail.com>
2021-09-26 23:37:53 +0200

    codesplitting pt2

--- src/clock_time_algorithm.rs
+++ src/clock_time_algorithm.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
-use crate::util;
 use crate::formatters;
+use crate::util;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 struct Clock<'a> {
     hours: &'a str,
@@ -64,23 +65,26 @@ pub fn clock_time_algorithm(input_clock_time: &str) -> String {
     .cloned()
     .collect();
 
-    let input_split: Vec<&str> = input_clock_time.split(':').collect();
+    let (clock_hours, clock_minutes): (&str, &str) =
+        input_clock_time.splitn(2, ":").collect_tuple().unwrap();
 
-    let clock = Clock {
-        hours: &input_split[0],
-        minutes: &input_split[1],
+    let clock: Clock = Clock {
+        hours: &clock_hours,
+        minutes: &clock_minutes,
     };
 
     let hours_as_int = clock.hours.parse::<i32>().unwrap();
 
     let parsed_hours = match hours_as_int {
-        hour if hour >= 12 => Some(hours_map[&formatters::hours_bigger_than_twelve_formatter(hour).as_str()]),
+        hour if hour >= 12 => {
+            Some(hours_map[&formatters::hours_bigger_than_twelve_formatter(hour).as_str()])
+        }
         hour if hour == 0 => Some(hours_map[(hour + 12).to_string().as_str()]),
         hour if hour < 10 => Some(hours_map[&format!("0{}", hour).to_string().as_str()]),
         hour => Some(hours_map[&format!("{}", hour).to_string().as_str()]),
     };
 
-    let minutes_as_int = clock.minutes.parse::<i32>().unwrap();
+    let minutes_as_int: i32 = clock.minutes.parse::<i32>().unwrap();
     let last_digit = minutes_as_int % 10;
     let first_digit = util::get_first_digit(minutes_as_int);
     let mut parsed_minute_tens = "";
@@ -89,15 +93,12 @@ pub fn clock_time_algorithm(input_clock_time: &str) -> String {
         parsed_minute_tens = minute_tens_map[format!("{}", first_digit).as_str()];
     }
 
-    let mut parsed_minute_;
-    parsed_minute_ = minutes_map[format!("0{}", last_digit).as_str()];
-
+    let parsed_minute_map_lookup = minutes_map[format!("0{}", last_digit).as_str()];
     let am_pm = if hours_as_int < 12 { "AM" } else { "PM" };
 
     // driver-code
     let mut time_string = String::new();
-
-    if minutes_as_int == 0 || parsed_minute_ == "zero" {
+    if minutes_as_int == 0 || parsed_minute_map_lookup == "zero" {
         if parsed_minute_tens != "" {
             time_string = format!(
                 "It's {} {} {}",
@@ -106,7 +107,6 @@ pub fn clock_time_algorithm(input_clock_time: &str) -> String {
                 am_pm
             );
         } else {
-            println!("parsed hours: {:?}", parsed_hours);
             time_string = format!("It's {} {}", parsed_hours.unwrap(), am_pm);
         }
     } else if parsed_minute_tens != "" && minutes_as_int > 9 {
@@ -114,27 +114,26 @@ pub fn clock_time_algorithm(input_clock_time: &str) -> String {
             "It's {} {} {} {}",
             parsed_hours.unwrap(),
             parsed_minute_tens,
-            parsed_minute_,
+            parsed_minute_map_lookup,
             am_pm
         );
     } else if minutes_as_int < 10 {
-        time_string = format!(
-            "It's {} oh {} {}",
-            parsed_hours.unwrap(),
-            parsed_minute_,
-            am_pm
-        );
+        time_string = formatters::oh_clock_formatter(parsed_hours, parsed_minute_map_lookup, am_pm);
     }
-
+    
     match time_string.is_empty() {
         true => panic!("time string never got its value."),
         false => println!("{}", time_string),
     };
-    
+
     return time_string;
 }
 
 #[test]
 fn test_10_am() {
     clock_time_algorithm("10:00");
+}
+#[test]
+fn test_23_13_pm() {
+    clock_time_algorithm("23:13");
 }
--- src/formatters.rs
+++ src/formatters.rs
@@ -1,13 +1,27 @@
 pub fn hours_bigger_than_twelve_formatter(hour: i32) -> String {
-  let hours_key: i32 = hour - 12;
-  if hours_key < 10 {
-      if hours_key < 0 {
-          return "0".to_string();
-      }
-      return format!("0{}", hours_key).to_string();
-  }
+    let hours_key: i32 = hour - 12;
+    if hours_key < 10 {
+        if hours_key < 0 {
+            return "0".to_string();
+        }
+        return format!("0{}", hours_key).to_string();
+    }
 
-  return format!("{}", &hours_key).to_string();
+    return format!("{}", &hours_key).to_string();
+}
+
+//todo: this function should take in a struct instead
+pub fn oh_clock_formatter(
+    parsed_hours: Option<&str>,
+    parsed_minute_map_lookup: &str,
+    am_pm: &str,
+) -> String {
+    return format!(
+        "It's {} oh {} {}",
+        parsed_hours.unwrap(),
+        parsed_minute_map_lookup,
+        am_pm
+    );
 }
 
 #[test]
@@ -15,4 +29,12 @@ fn test_hour_formatter() {
     assert_eq!(hours_bigger_than_twelve_formatter(13), "01");
     assert_eq!(hours_bigger_than_twelve_formatter(23), "11");
     assert_eq!(hours_bigger_than_twelve_formatter(0), "0");
+}
+
+#[test]
+fn oh_clock_formatter_test() {
+    let parsed_hours = Some("four");
+    let parsed_minutes = "five";
+    let under_test = oh_clock_formatter(parsed_hours, parsed_minutes, "PM");
+    assert_eq!(under_test, "It's four oh five PM");
 }
